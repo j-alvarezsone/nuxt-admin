8b37dfe34ca4a4fc12af65862c57e6d7abc13ccd
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-12-25 22:36:28 +0100

    refactor: enhance eslint configuration with custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt();
-// Your custom configs here
+export default withNuxt({
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'vue/block-order': [
+      'error',
+      {
+        order: ['script', 'template', 'style'],
+      },
+    ],
+  },
+});
