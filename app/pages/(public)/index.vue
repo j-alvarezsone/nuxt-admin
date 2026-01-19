b84dda2196b79a73ec808a914e187a7f788531c9
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2026-01-19 19:36:39 +0100

    feat(testimonials): refactor testimonials component to use props and update API for fetching reviews

--- app/pages/(public)/index.vue
+++ app/pages/(public)/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-
+  const { data: testimonials } = await useFetch('/api/home/reviews')
 </script>
 
 <template>
   <div class="flex flex-col gap-6">
     <HomeCTA />
     <HomeFeatures />
-    <HomeTestimonials />
+    <HomeTestimonials v-if="testimonials" :testimonials />
     <HomeStack />
   </div>
 </template>
---
+++ server/api/home/reviews.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler(async (_event) => {
+  const reviews = await prisma.siteReview.findMany({
+    take: 10,
+    orderBy: {
+      id: 'desc',
+    },
+    select: {
+      name: true,
+      subtitle: true,
+      description: true,
+      profileImage: true,
+    }
+  })
+
+  return reviews
+})
