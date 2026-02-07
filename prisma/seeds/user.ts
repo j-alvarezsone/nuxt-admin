f7f999a30cc0131ed245569a9f5ad124b6cdd815
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2026-02-07 11:26:19 +0100

    feat(seeds): hash passwords for user seeding functionality

--- prisma/seeds/user.ts
+++ prisma/seeds/user.ts
@@ -1,10 +1,19 @@
+import bcrypt from "bcryptjs";
 import type { PrismaClient } from '../generated/client';
 import { users } from '../../shared/mock/user';
 
 export async function seedUsers(prisma: PrismaClient) {
   await prisma.user.deleteMany();
 
+  const usersWithHashedPassword = users.map((user) => {
+    const hashedPassword = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+    return {
+      ...user,
+      password: hashedPassword,
+    };
+  });
+
   await prisma.user.createMany({
-    data: users,
+    data: usersWithHashedPassword,
   });
 }
